0c258406b63402851876ff20ddbf4575c38c2209
Om7866 <omgohel7866@gmail.com>
2025-04-09 18:46:30 +0530

    Updated favicon and modified product-related components

--- src/Config/Api.ts
+++ src/Config/Api.ts
@@ -5,7 +5,8 @@ export const DEPLOYED_URL = "https://whitfordwardobe-backend.onrender.com"
 // change api
 
 export const api = axios.create({
-  baseURL: API_URL, 
+  baseURL: DEPLOYED_URL, 
+
   headers: {
     'Content-Type': 'application/json',
   },
